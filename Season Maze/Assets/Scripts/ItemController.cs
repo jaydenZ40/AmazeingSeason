97dc821d36f8d1c1c42c3c4faa3b65177a03c60c
jaydenZ40 <zjp981310647@gmail.com>
2019-02-15 11:08:16 -0800

    add player movement. pickup and drop item

---
+++ Season Maze/Assets/Scripts/ItemController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    private bool isFollowingPlayer = false;
+
+
+    void Start()
+    {
+        PlayerController.instance.onOperateItem.AddListener(OperateItem);
+    }
+    void OperateItem()
+    {
+        if (!isFollowingPlayer)
+        {
+            this.transform.position = PlayerController.instance.rb.transform.position + new Vector3(0, 0.5f, 0);
+            this.transform.SetParent(PlayerController.instance.transform);
+            PlayerController.instance.itemName = this.name;
+            // hold the item above the player?
+        }
+        else
+        {
+            this.transform.parent = null;
+        }
+        isFollowingPlayer = !isFollowingPlayer;
+    }
+}
---
+++ Season Maze/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController instance;
+    public Rigidbody2D rb;
+    public float moveSpeed = 2.5f;
+    public string itemName = "";
+    public UnityEvent onWrapGate = new UnityEvent();
+    public UnityEvent onOperateItem = new UnityEvent();
+
+
+
+    void Awake()
+    {
+        instance = this;
+        rb = this.transform.GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.transform.Translate(new Vector2(-1 * Time.deltaTime * moveSpeed, 0));
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.transform.Translate(new Vector2(1 * Time.deltaTime * moveSpeed, 0));
+        }
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.transform.Translate(new Vector2(0, 1 * Time.deltaTime * moveSpeed));
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.transform.Translate(new Vector2(0, -1 * Time.deltaTime * moveSpeed));
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && itemName != "")
+        {
+            onOperateItem.Invoke();  // drop down the item, pass event to ItemController maybe
+
+            // clear the item box?
+
+            itemName = "";
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (itemName == "") // cannot pick two or more items at same time?
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && other.transform.CompareTag("Item"))
+            {
+                onOperateItem.Invoke(); // pick up the item,  pass event to in ItemController maybe
+
+                // show it on an item box?
+
+                itemName = other.transform.name;
+            }
+        }
+        if (other.transform.CompareTag("WrapGate"))
+        {
+            onWrapGate.Invoke(); // send to levelController to show options of 4 seasons in a panel
+        }
+    }
+}
